e84a54bb54dfe53c22cca68c434b584329448b3c
syed abdul hakim <s.abdulhakim10@gmail.com>
2022-11-25 01:15:58 +0600

    create API to get car info by id

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 const express = require('express');
 const cors = require('cors');
-const app = express();
 const { MongoClient, ServerApiVersion } = require('mongodb');
 require('dotenv').config();
 
 
+const app = express();
 const port = process.env.PORT || 5000;
 
 // middle wares
@@ -14,9 +14,39 @@ app.use(express.json());
 
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.47nvmfs.mongodb.net/?retryWrites=true&w=majority`;
-console.log(uri);
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
+async function run(){
+    try{
+        const carCollection = client.db('swapcarsDb').collection('cars');
+        const categoryCollection = client.db('swapcarsDb').collection('categories');
+        
+        // get all cars
+        app.get('/cars', async(req, res) => {
+            const query = {};
+            const cars = await carCollection.find(query).toArray();
+            res.send(cars);
+        })
+        // get car info by id
+        app.get('/categories/:id', async(req, res) => {
+            const id = req.params.id;
+            const query = {category_id: id};
+            const cars = await carCollection.find(query).toArray();
+            res.send(cars);
+        })
+
+        // get all cars
+        app.get('/categories', async(req, res) => {
+            const query = {};
+            const categories = await categoryCollection.find(query).toArray();
+            res.send(categories);
+        })
+    }
+    finally{
+
+    }
+}
+run().catch(err => console.error(err));
 
 app.get('/', (req, res) => {
     res.send('swapcars server is running');
